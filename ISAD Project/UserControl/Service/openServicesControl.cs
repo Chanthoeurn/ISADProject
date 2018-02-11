3195d3d9e19a21a6388701bb986833fc8f50abd5
chanthoeurn <chanthoeurnchhim168@gmail.com>
2018-02-11 16:22:47 +0700

    Add openServiceControl

---
+++ ISAD Project/UserControl/Service/openServicesControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISAD_Project
+{
+    public partial class openServicesControl : UserControl
+    {
+        public openServicesControl()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSearchService_TextChanged(object sender, EventArgs e)
+        {
+            if (txtSearchService.Text =="") 
+            {
+                txtSearchService.Text = "Search Services here...";
+                txtSearchService.ForeColor = Color.Silver;
+            }
+        }
+    }
+}
--- ISAD Project/formMain.Designer.cs
+++ ISAD Project/formMain.Designer.cs
@@ -56,38 +56,12 @@
             this.label6 = new System.Windows.Forms.Label();
             this.lblUser = new System.Windows.Forms.Label();
             this.btnSignOut = new System.Windows.Forms.Button();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.panel11 = new System.Windows.Forms.Panel();
-            this.label8 = new System.Windows.Forms.Label();
-            this.panel9 = new System.Windows.Forms.Panel();
-            this.label7 = new System.Windows.Forms.Label();
-            this.panel8 = new System.Windows.Forms.Panel();
-            this.btnUpdateServices = new System.Windows.Forms.Button();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.panel7 = new System.Windows.Forms.Panel();
-            this.panel10 = new System.Windows.Forms.Panel();
-            this.label11 = new System.Windows.Forms.Label();
-            this.btnViewServices = new System.Windows.Forms.Button();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.panel6 = new System.Windows.Forms.Panel();
-            this.btnOpenServices = new System.Windows.Forms.Button();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureImageUser)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel5.SuspendLayout();
-            this.panel11.SuspendLayout();
-            this.panel9.SuspendLayout();
-            this.panel8.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            this.panel7.SuspendLayout();
-            this.panel10.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            this.panel6.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -452,192 +426,12 @@
             this.btnSignOut.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnSignOut.UseVisualStyleBackColor = true;
             // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(254)))), ((int)(((byte)(255)))));
-            this.panel5.Controls.Add(this.panel11);
-            this.panel5.Controls.Add(this.panel9);
-            this.panel5.Controls.Add(this.panel8);
-            this.panel5.Controls.Add(this.panel7);
-            this.panel5.Controls.Add(this.panel6);
-            this.panel5.Location = new System.Drawing.Point(279, 104);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(967, 502);
-            this.panel5.TabIndex = 7;
-            // 
-            // panel11
-            // 
-            this.panel11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.panel11.Controls.Add(this.label8);
-            this.panel11.Location = new System.Drawing.Point(673, 135);
-            this.panel11.Name = "panel11";
-            this.panel11.Size = new System.Drawing.Size(196, 37);
-            this.panel11.TabIndex = 4;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Ubuntu", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(35)))), ((int)(((byte)(62)))));
-            this.label8.Location = new System.Drawing.Point(25, 2);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(149, 24);
-            this.label8.TabIndex = 0;
-            this.label8.Text = "Update Services";
-            // 
-            // panel9
-            // 
-            this.panel9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.panel9.Controls.Add(this.label7);
-            this.panel9.Location = new System.Drawing.Point(95, 135);
-            this.panel9.Name = "panel9";
-            this.panel9.Size = new System.Drawing.Size(196, 37);
-            this.panel9.TabIndex = 3;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Ubuntu", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(35)))), ((int)(((byte)(62)))));
-            this.label7.Location = new System.Drawing.Point(33, 2);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(133, 24);
-            this.label7.TabIndex = 0;
-            this.label7.Text = "Open Services";
-            // 
-            // panel8
-            // 
-            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(47)))), ((int)(((byte)(73)))));
-            this.panel8.Controls.Add(this.btnUpdateServices);
-            this.panel8.Controls.Add(this.pictureBox5);
-            this.panel8.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.panel8.ForeColor = System.Drawing.Color.Silver;
-            this.panel8.Location = new System.Drawing.Point(673, 135);
-            this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(196, 238);
-            this.panel8.TabIndex = 4;
-            // 
-            // btnUpdateServices
-            // 
-            this.btnUpdateServices.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(35)))), ((int)(((byte)(62)))));
-            this.btnUpdateServices.FlatAppearance.BorderSize = 0;
-            this.btnUpdateServices.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
-            this.btnUpdateServices.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnUpdateServices.ForeColor = System.Drawing.Color.Transparent;
-            this.btnUpdateServices.Location = new System.Drawing.Point(0, 203);
-            this.btnUpdateServices.Name = "btnUpdateServices";
-            this.btnUpdateServices.Size = new System.Drawing.Size(196, 35);
-            this.btnUpdateServices.TabIndex = 2;
-            this.btnUpdateServices.Text = "Update";
-            this.btnUpdateServices.UseVisualStyleBackColor = false;
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(37, 65);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(132, 136);
-            this.pictureBox5.TabIndex = 1;
-            this.pictureBox5.TabStop = false;
-            // 
-            // panel7
-            // 
-            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(47)))), ((int)(((byte)(73)))));
-            this.panel7.Controls.Add(this.panel10);
-            this.panel7.Controls.Add(this.btnViewServices);
-            this.panel7.Controls.Add(this.pictureBox4);
-            this.panel7.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.panel7.ForeColor = System.Drawing.Color.Silver;
-            this.panel7.Location = new System.Drawing.Point(386, 135);
-            this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(196, 238);
-            this.panel7.TabIndex = 3;
-            // 
-            // panel10
-            // 
-            this.panel10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.panel10.Controls.Add(this.label11);
-            this.panel10.Location = new System.Drawing.Point(0, 0);
-            this.panel10.Name = "panel10";
-            this.panel10.Size = new System.Drawing.Size(196, 37);
-            this.panel10.TabIndex = 4;
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Ubuntu", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(35)))), ((int)(((byte)(62)))));
-            this.label11.Location = new System.Drawing.Point(35, 2);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(126, 24);
-            this.label11.TabIndex = 0;
-            this.label11.Text = "View Services";
-            // 
-            // btnViewServices
-            // 
-            this.btnViewServices.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(35)))), ((int)(((byte)(62)))));
-            this.btnViewServices.FlatAppearance.BorderSize = 0;
-            this.btnViewServices.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
-            this.btnViewServices.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnViewServices.ForeColor = System.Drawing.Color.Transparent;
-            this.btnViewServices.Location = new System.Drawing.Point(0, 203);
-            this.btnViewServices.Name = "btnViewServices";
-            this.btnViewServices.Size = new System.Drawing.Size(196, 35);
-            this.btnViewServices.TabIndex = 2;
-            this.btnViewServices.Text = "View";
-            this.btnViewServices.UseVisualStyleBackColor = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(37, 65);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(132, 136);
-            this.pictureBox4.TabIndex = 1;
-            this.pictureBox4.TabStop = false;
-            // 
-            // panel6
-            // 
-            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(47)))), ((int)(((byte)(73)))));
-            this.panel6.Controls.Add(this.btnOpenServices);
-            this.panel6.Controls.Add(this.pictureBox3);
-            this.panel6.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.panel6.ForeColor = System.Drawing.Color.Silver;
-            this.panel6.Location = new System.Drawing.Point(95, 135);
-            this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(196, 238);
-            this.panel6.TabIndex = 0;
-            // 
-            // btnOpenServices
-            // 
-            this.btnOpenServices.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(35)))), ((int)(((byte)(62)))));
-            this.btnOpenServices.FlatAppearance.BorderSize = 0;
-            this.btnOpenServices.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
-            this.btnOpenServices.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnOpenServices.ForeColor = System.Drawing.Color.Transparent;
-            this.btnOpenServices.Location = new System.Drawing.Point(0, 203);
-            this.btnOpenServices.Name = "btnOpenServices";
-            this.btnOpenServices.Size = new System.Drawing.Size(196, 35);
-            this.btnOpenServices.TabIndex = 2;
-            this.btnOpenServices.Text = "OPEN";
-            this.btnOpenServices.UseVisualStyleBackColor = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(37, 65);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(132, 136);
-            this.pictureBox3.TabIndex = 1;
-            this.pictureBox3.TabStop = false;
-            // 
             // formMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1294, 660);
-            this.Controls.Add(this.panel5);
             this.Controls.Add(this.btnSignOut);
             this.Controls.Add(this.lblUser);
             this.Controls.Add(this.label6);
@@ -662,19 +456,6 @@
             this.panel4.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureImageUser)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel5.ResumeLayout(false);
-            this.panel11.ResumeLayout(false);
-            this.panel11.PerformLayout();
-            this.panel9.ResumeLayout(false);
-            this.panel9.PerformLayout();
-            this.panel8.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            this.panel7.ResumeLayout(false);
-            this.panel10.ResumeLayout(false);
-            this.panel10.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            this.panel6.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -707,23 +488,7 @@
         private System.Windows.Forms.Label lblUser;
         private System.Windows.Forms.Button btnSignOut;
         private System.Windows.Forms.PictureBox pictureImageUser;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Button btnOpenServices;
-        private System.Windows.Forms.Panel panel8;
-        private System.Windows.Forms.Button btnUpdateServices;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.Panel panel7;
-        private System.Windows.Forms.Button btnViewServices;
-        private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox6;
-        private System.Windows.Forms.Panel panel9;
-        private System.Windows.Forms.Panel panel11;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Panel panel10;
-        private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Panel slidePanel;
     }
 }
